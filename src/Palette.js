73184cb5f52136ee2734d682278e359850aaa6e7
khalid alasmary <khalid@alasmary.com>
2019-08-01 11:43:56 +0300

    add palette footer

--- src/Palette.js
+++ src/Palette.js
@@ -20,20 +20,24 @@ class Pallete extends Component {
   };
 
   render() {
+    const { colors, paletteName, emoji } = this.props.palette;
     const { level, format } = this.state;
-    const colorBoxes = this.props.palette.colors[level].map(color => (
-      <ColorBox key={color.hex} name={color.name} background={color[format]} />
+    const colorBoxes = colors[level].map(color => (
+      <ColorBox key={color.id} name={color.name} background={color[format]} />
     ));
     return (
       <div className='Palette'>
+        {/* Nav */}
         <Navbar
           level={level}
           onLevelChange={this.onLevelChange}
           handleChange={this.onColorFormatChange}
         />
-        {/* Nav */}
         <div className='Palette-colors'>{colorBoxes}</div>
         {/* footer */}
+        <footer className='palette-footer'>
+          {paletteName} <span className='footer-emoji'>{emoji}</span>
+        </footer>
       </div>
     );
   }
