bb851578025cb6bf5c156ecf2f7c46642a5644f5
khalid alasmary <khalid@alasmary.com>
2019-08-17 14:13:59 +0300

    Updates

---
+++ precache-manifest.a332ae8dac3642c43a11318e856ef1f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded634c01678db0cff30103e1ee802c6",
+    "url": "/color-picker/index.html"
+  },
+  {
+    "revision": "8b1fdef084c9e07dbd8a",
+    "url": "/color-picker/static/css/2.e8e75e1a.chunk.css"
+  },
+  {
+    "revision": "e7e742106f270e9ab813",
+    "url": "/color-picker/static/css/main.458f29da.chunk.css"
+  },
+  {
+    "revision": "8b1fdef084c9e07dbd8a",
+    "url": "/color-picker/static/js/2.328d17b2.chunk.js"
+  },
+  {
+    "revision": "e7e742106f270e9ab813",
+    "url": "/color-picker/static/js/main.94b7f09c.chunk.js"
+  },
+  {
+    "revision": "a7d8e61640eb3f8830d1",
+    "url": "/color-picker/static/js/runtime~main.3187d74d.js"
+  },
+  {
+    "revision": "11ce659f9d40c528c907db8089168a04",
+    "url": "/color-picker/static/media/bg.11ce659f.svg"
+  }
+]);
